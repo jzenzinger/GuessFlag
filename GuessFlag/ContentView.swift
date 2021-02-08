6d380500ec8868f4c46b1e3c43ec97670ebd919c
jzenzinger <juris.zen@seznam.cz>
2021-02-08 21:08:29 +0100

    Initial Commit

---
+++ GuessFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessFlag
+//
+//  Created by Jiří Zenzinger on 08.02.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessFlag/GuessFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessFlagApp.swift
+//  GuessFlag
+//
+//  Created by Jiří Zenzinger on 08.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct GuessFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
